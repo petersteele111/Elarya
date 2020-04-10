4a2aabb4beb9685b953740719bf8aa574556667b
Peter Steele <steelep@mail.nmc.edu>
2020-04-09 21:45:46 -0400

    Added Location Class

---
+++ Elarya/Models/Location.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elarya.Models
+{
+    class Location
+    {
+        #region Fields
+
+        private int _id;
+		private string _name;
+		private string _description;
+		private bool _accessible;
+
+		#endregion
+
+		#region Properties
+
+		public int ID
+		{
+			get => _id;
+			set
+			{
+				_id = value;
+			}
+		}
+
+		
+		public string Name
+		{
+			get => _name;
+			set 
+			{
+				_name = value;
+			}
+		}
+
+		public string Description
+		{
+			get => _description;
+			set
+			{
+				_description = value;
+			}
+		}
+
+		public bool Accessible
+		{
+			get => _accessible;
+			set
+			{
+				_accessible = value;
+			}
+		}
+
+		#endregion
+	}
+}
