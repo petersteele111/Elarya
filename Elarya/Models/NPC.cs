b12f24154c1d28336232ebc5873f77df07548ce1
Peter Steele <steelep@mail.nmc.edu>
2020-04-23 22:04:33 -0400

    Created the NPC Abstract Class

---
+++ Elarya/Models/NPC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elarya.Models
+{
+    public abstract class NPC : Character
+    {
+        public string Description { get; set; }
+        
+        public string Information
+        {
+            get => InformationText();
+            set
+            {
+
+            }
+        }
+
+        public NPC()
+        {
+
+        }
+
+        public NPC(int id, int locationId, string name, int age, RaceType race, GenderType gender, string description) : base(id, locationId, name, age, race, gender)
+        {
+            Id = id;
+            LocationId = locationId;
+            Age = age;
+            Race = race;
+            Gender = gender;
+            Description = description;
+        }
+
+        protected abstract string InformationText();
+
+        public override bool HasQuest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
