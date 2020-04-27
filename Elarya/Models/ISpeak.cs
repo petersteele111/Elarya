1ac07441fef9ba4e43badee10274e15a85bb957d
Peter Steele <steelep@mail.nmc.edu>
2020-04-26 22:20:34 -0400

    ISpeak interface cleanedup

--- Elarya/Models/ISpeak.cs
+++ Elarya/Models/ISpeak.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Elarya.Models
 {
-    interface ISpeak
+    internal interface ISpeak
     {
         List<string> Messages { get; set; }
 
