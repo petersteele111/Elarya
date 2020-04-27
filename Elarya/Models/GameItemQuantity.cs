f2d83e7d9b8883d149bf2d82df1e0903ba7bfcde
Peter Steele <steelep@mail.nmc.edu>
2020-04-26 22:17:32 -0400

    GameItemQuantities Class Cleanedup

--- Elarya/Models/GameItemQuantity.cs
+++ Elarya/Models/GameItemQuantity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Elarya.Models
+﻿namespace Elarya.Models
 {
     public class GameItemQuantity
     {
